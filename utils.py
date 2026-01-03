b638eafba3d1a558ca59d9918b5611dd7c84a453
loplxl <lop.crystallisedsatisfaction.lxl@proton.me>
2026-01-03 22:53:14 +0000

    add auto timer res

--- utils.py
+++
@@ -1,8 +0,0 @@
-from os.path import join,abspath
-import sys
-def resource_path(relative_path):
-    if getattr(sys, 'frozen', False):  # running as PyInstaller
-        base_path = sys._MEIPASS
-    else:
-        base_path = abspath(".")
-    return join(base_path, relative_path)
